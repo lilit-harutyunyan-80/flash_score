f1337db6f42c37a489f0f55195b98d95c6c5e7af
Lenovo <2838lilitmane@gmail.com>
2023-06-17 21:01:15 +0400

    Initial commit

---
+++ src/main/java/am/itspace/flash_score/FlashScoreApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.flash_score;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashScoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/itspace/flash_score/FlashScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.flash_score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
