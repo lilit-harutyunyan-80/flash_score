faaf8c7f9c05d5aee5ae11e74f63fe258966bd1c
Lenovo <2838lilitmane@gmail.com>
2023-07-05 19:13:14 +0400

    Contact and leagueController

---
+++ src/main/java/am/itspace/flash_score/controller/ContactController.java
@@ -0,0 +1,12 @@
+package am.itspace.flash_score.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+    @GetMapping("/contact")
+    public String contactPage(){
+        return "contact";
+    }
+}
